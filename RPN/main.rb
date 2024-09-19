71adcea40ff6a131635dadf7a500e4476d698719
Ivan Mihun <vsk76090@gmail.com>
2024-09-19 19:27:28 +0200

    basic converting function

--- RPN/main.rb
+++ RPN/main.rb
@@ -1,7 +1,18 @@
 # frozen_string_literal: true
 
 def get_postfix_notation(infix_expression)
-  nil
+  operator_priorities = { 1 => %w[* /], 2 => %w[- +] }
+  validate_infix_expression(infix_expression)
+  expression_elements = infix_expression.gsub(' ', '').scan(/\d+|\S/)
+  operator_priorities.keys.each do |priority|
+    expression_elements.each_with_index do |element, index|
+      if operator_priorities[priority].include? element
+        expression_elements[index - 1] += expression_elements[index + 1] + expression_elements[index]
+        expression_elements -= [expression_elements[index + 1], expression_elements[index]]
+      end
+    end
+  end
+  expression_elements[0]
 end
 
 def validate_infix_expression(infix_expression)
@@ -11,7 +22,7 @@ end
 def main
   print "Please enter your expression: "
   infix_expression = gets.chomp
-  puts "You entered: #{infix_expression}"
+  puts get_postfix_notation infix_expression
 end
 
 main
