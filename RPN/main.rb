0ad0c2ec6e3a4d7357dcfd6b66cd4e0c94920208
Ivan Mihun <vsk76090@gmail.com>
2024-09-19 16:50:29 +0200

    rpn init

---
+++ RPN/main.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+def get_postfix_notation(infix_expression)
+  nil
+end
+
+def validate_infix_expression(infix_expression)
+  nil
+end
+
+def main
+  print "Please enter your expression: "
+  infix_expression = gets.chomp
+  puts "You entered: #{infix_expression}"
+end
+
+main
