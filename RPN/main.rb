0fbf279e649ac8fcae8ae695aded076431b1ae34
Ivan Mihun <vsk76090@gmail.com>
2024-09-20 19:46:12 +0200

    add function that finds the start and end indices of all the substrings that are situated within the top level parentheses

--- RPN/main.rb
+++ RPN/main.rb
@@ -2,6 +2,32 @@
 
 class NotExpressionError < StandardError; end
 
+class ParenthesesError < StandardError; end
+
+def validate_infix_expression(string)
+  pattern = %r"^\s*\d+(\s*[+\-*/]\s*\d+\s*)*$"
+  unless string =~ pattern
+    raise NotExpressionError.new "#{string} isn't a valid expression"
+  end
+end
+
+def find_substrings_indexes_within_parentheses(string)
+  parentheses_indexes = []
+  open_parentheses = 0
+  string.each_char.with_index do |char, index|
+    if char == '('
+      parentheses_indexes << index << -1 if open_parentheses.zero?
+      open_parentheses += 1
+    elsif char == ')'
+      open_parentheses -= 1
+      parentheses_indexes[-1] = index if open_parentheses >= 0
+      raise(ParenthesesError, "Closing parenthesis is not preceded by an opening parenthesis") if open_parentheses < 0
+    end
+  end
+  raise(ParenthesesError, "Opening parenthesis is not closed") unless open_parentheses.zero?
+  parentheses_indexes.each_slice(2).to_a.each(&:freeze).freeze
+end
+
 def get_postfix_notation(infix_expression)
   operator_priorities = { 1 => %w[* /], 2 => %w[- +] }
   validate_infix_expression(infix_expression)
@@ -16,17 +42,12 @@ def get_postfix_notation(infix_expression)
   expression_elements[0]
 end
 
-def validate_infix_expression(string)
-  pattern = %r"^\s*\d+(\s*[+\-*/]\s*\d+\s*)*$"
-  unless string =~ pattern
-    raise NotExpressionError.new "#{string} isn't a valid expression"
-  end
-end
-
 def main
   print "Please enter your expression: "
   infix_expression = gets.chomp
-  puts get_postfix_notation infix_expression
+  substrings_indexes = get_indexes_of_substrings_within_parentheses infix_expression
+  substrings_indexes.each { |indexes| puts infix_expression[indexes[0]..indexes[1]] }
+  # puts get_postfix_notation infix_expression
 end
 
 main
