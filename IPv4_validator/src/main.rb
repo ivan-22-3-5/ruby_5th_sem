a04952fef263dccedab882e3f85f0b9e1d4d738e
Ivan Mihun <vsk76090@gmail.com>
2024-10-04 13:32:32 +0200

    add user input

--- IPv4_validator/src/main.rb
+++ IPv4_validator/src/main.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
+require_relative 'ip_validator'
+
 def main
-  nil
+  puts "Enter a string: "
+  puts "Your string is #{"not " unless ipv4?(gets.chomp)}an IPv4 address"
 end
 
 if __FILE__ == $0
