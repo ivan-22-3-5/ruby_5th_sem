a0f174ded76aab38b4a2f1fdcbb21095a92caf12
Ivan Mihun <vsk76090@gmail.com>
2024-10-14 12:17:18 +0200

    add: csv_writer test fix: file isn't created when data is invalid

---
+++ CSV_conversion_rates/spec/csv_writer_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+require_relative '../src/csv_writer'
+
+RSpec.describe CSVWriter do
+  include CSVWriter
+
+  describe '#write_csv' do
+    let(:filename) { 'files/test_currency_rates.csv' }
+    let(:header) { %w[Currency USD] }
+    let(:data) { [['EUR', 1.12], ['GBP', 1.31]] }
+
+    it 'creates a CSV file with correct content' do
+      write_csv(filename, header, data)
+
+      expect(File.exist?(filename)).to be true
+      expect(File.read(filename)).to include('Currency,USD')
+      expect(File.read(filename)).to include('EUR,1.12')
+      expect(File.read(filename)).to include('GBP,1.31')
+
+      File.delete(filename) if File.exist?(filename)
+    end
+
+    it 'raises an error if row length does not match header length' do
+      invalid_data = [['EUR', 0.84], ['GBP']]
+
+      expect {
+        write_csv(filename, header, invalid_data)
+      }.to raise_error(ArgumentError, 'All rows must have the same length as the header')
+    end
+  end
+end
--- CSV_conversion_rates/src/csv_writer.rb
+++ CSV_conversion_rates/src/csv_writer.rb
@@ -2,12 +2,10 @@
 require 'csv'
 module CSVWriter
   def write_csv(filename, header, data)
+    raise ArgumentError, "All rows must have the same length as the header" if data.any? { |r| r.length != header.length }
     CSV.open(filename, "w") do |csv|
       csv << header
-      data.each { |row|
-        raise ArgumentError, "All rows must have the same length as the header" if row.length != header.length
-        csv << row
-      }
+      data.each { |row| csv << row }
     end
   end
 end
