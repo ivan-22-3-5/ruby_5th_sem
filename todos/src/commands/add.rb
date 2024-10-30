9f0894aa46f1fc640fe54f2f441a2af606bd9eaa
Ivan Mihun <vsk76090@gmail.com>
2024-10-30 18:34:01 +0100

    fix(todos): change year format from yyyy to yy

--- todos/src/commands/add.rb
+++ todos/src/commands/add.rb
@@ -14,15 +14,15 @@ class TodosApp < Thor
 
   def add(title)
     begin
-      date = options[:date] ? Utils.parse_datetime(options[:date], '%d.%m', '%d.%m.%Y') : DateTime.now
+      date = options[:date] ? Utils.parse_datetime(options[:date],  '%d.%m.%y', '%d.%m') : DateTime.now
       time = options[:time] ? Utils.parse_datetime(options[:time], '%H:%M') : DateTime.new(0)
       deadline = DateTime.new(date.year, date.month, date.day, time.hour, time.min)
 
       Todos.add({ 'title' => title, 'completed' => false, 'deadline' => deadline })
-      puts "Added todo '#{title}' with deadline #{deadline.strftime('%d.%m.%Y %H:%M')}".green
+      puts "Added todo '#{title}' with deadline #{deadline.strftime('%d.%m.%y %H:%M')}".green
 
     rescue Date::Error
-      puts "[ERROR]: Invalid datetime format. Use 'dd.mm' or 'dd.mm.yyyy' for date and 'hh:mm' for time.".red
+      puts "[ERROR]: Invalid datetime format. Use 'dd.mm' or 'dd.mm.yy' for date and 'hh:mm' for time.".red
     rescue Todos::AlreadyExistsError
       puts "[ERROR]: Todo with the title #{title} already exists".red
     end
