0dbe3def0637c48143c593f8538133b1fe1a438f
Ivan Mihun <vsk76090@gmail.com>
2024-10-30 17:17:58 +0100

    feat(todos): add todo filtering

--- todos/src/commands/show.rb
+++ todos/src/commands/show.rb
@@ -5,11 +5,24 @@ require_relative '../todos'
 require_relative '../utils'
 
 using Rainbow
+
 class TodosApp < Thor
   desc "show", "shows todos"
+
+  method_option :completed, type: :boolean, default: nil, aliases: :c
+  method_option :days, type: :numeric, default: nil, aliases: :d
+  method_option :expired, type: :boolean, default: nil, aliases: :e
+
   def show
     todos = Todos.all
+
+    query = [
+      options[:completed].nil? || proc { |todo| todo['completed'] == options[:completed] },
+      options[:expired].nil? || proc { |todo| (DateTime.parse(todo['deadline']) - DateTime.now) <= 0 == options[:expired] },
+      options[:days].nil? || proc { |todo| (DateTime.parse(todo['deadline']) - DateTime.now) <= options[:days] }
+    ].reject { |q| q == true }
+
+    puts Utils.build_table(todos.select { |todo| query.all? { |q| q.call(todo) } }) unless todos.empty?
     puts "No todos found".blue if todos.empty?
-    puts Utils.build_table(todos) unless todos.empty?
   end
 end
