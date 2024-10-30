050a34c4e1c5b1db6196af79e0f48238300106a0
Ivan Mihun <vsk76090@gmail.com>
2024-10-30 08:32:44 +0100

    feat(todos): add exception throwing when task already exists

--- todos/src/commands/add.rb
+++ todos/src/commands/add.rb
@@ -6,6 +6,7 @@ require 'rainbow/refinement'
 require_relative '../utils'
 require_relative '../todos'
 using Rainbow
+
 class TodosApp < Thor
   desc "add NAME", "Adds todo"
   method_option :date, type: :string, default: nil, aliases: :d
@@ -13,13 +14,19 @@ class TodosApp < Thor
 
   def add(name)
     begin
-      date = date ? parse_datetime(date, '%d.%m', '%d.%m.%Y'): DateTime.now
-      time = time ? parse_datetime(time, '%H:%M') : DateTime.new(0)
-      deadline =  DateTime.new(date.year, date.month, date.day, time.hour, time.min)
+      date = options[:date] ? Utils.parse_datetime(options[:date], '%d.%m', '%d.%m.%Y') : DateTime.now
+      time = options[:time] ? Utils.parse_datetime(options[:time], '%H:%M') : DateTime.new(0)
+      deadline = DateTime.new(date.year, date.month, date.day, time.hour, time.min)
     rescue Date::Error
       puts "[ERROR]: Invalid datetime format. Use 'dd.mm' or 'dd.mm.yyyy' for date and 'hh:mm' for time.".red
       return
     end
-    Todos.add({ name: name, completed: false, deadline: deadline })
+
+    begin
+      Todos.add({ 'name' => name, 'completed' => false, 'deadline' => deadline })
+      puts "Added todo '#{name}' with deadline #{deadline.strftime('%d.%m.%Y %H:%M')}".green
+    rescue Todos::AlreadyExistsError
+      puts "[ERROR]: Todo with name #{name} already exists".red
+    end
   end
 end
--- todos/src/todos.rb
+++ todos/src/todos.rb
@@ -1,10 +1,16 @@
 module Todos
+  AlreadyExistsError = Class.new(StandardError)
+  FILENAME = 'todos.json'
   def self.add(todo)
-    json_string = File.read('todos.json') rescue '[]'
-    todos = JSON.parse(json_string) << todo
+    json_string = File.read(FILENAME) rescue '[]'
+    todos = JSON.parse(json_string)
 
-    File.open('todos.json', 'w') do |f|
-      JSON.dump(todos, f)
+    if todos.any? { |t| t['name'] == todo['name'] }
+      raise AlreadyExistsError, "Todo with name #{todo['name']} already exists"
+    end
+
+    File.open(FILENAME, 'w') do |f|
+      JSON.dump(todos + [todo], f)
     end
   end
 end
