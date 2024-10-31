3849df1b31cc707f47531c7ca781910710eb25ba
Ivan Mihun <vsk76090@gmail.com>
2024-10-31 11:26:28 +0100

    feat(todos): colorize deadline and completeness status

--- todos/src/utils.rb
+++ todos/src/utils.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 require 'date'
 require 'terminal-table'
+require 'rainbow/refinement'
+
+using Rainbow
 
 module Utils
   def self.parse_datetime(str, *formats)
@@ -14,10 +17,18 @@ module Utils
 
   def self.build_table(todos)
     rows = todos.map { |todo| [todo['title'],
-                               todo['deadline'].nil? ? 'no deadline' : DateTime.parse(todo['deadline']).strftime('%d.%m.%Y %H:%M'),
-                               todo['completed']] }
+                               build_deadline_string(todo['deadline']),
+                               todo['completed'] ? 'yes'.green : 'no'.red] }
     table = Terminal::Table.new :title => "todos", :headings => %w[title deadline completed], :rows => rows
     table.style = { :border => :unicode_thick_edge }
     table
+  end
+
+  private_class_method def self.build_deadline_string(deadline)
+    return 'no deadline'.green if deadline.nil?
+
+    datetime = DateTime.parse(deadline)
+    formatted_deadline = datetime.strftime('%d.%m.%Y %H:%M')
+    datetime > DateTime.now ? formatted_deadline.green : formatted_deadline.red
   end
 end
