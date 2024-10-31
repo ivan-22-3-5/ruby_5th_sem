accdfccfa8f31a8808b0b01c9165207a4c448cd1
Ivan Mihun <vsk76090@gmail.com>
2024-10-31 11:35:58 +0100

    feat(todos): configure exit_on_failure to exit without an error

--- todos/src/app.rb
+++ todos/src/app.rb
@@ -3,3 +3,9 @@ require_relative 'commands/list'
 require_relative 'commands/complete'
 require_relative 'commands/remove'
 require_relative 'commands/clear'
+
+class TodosApp < Thor
+  def self.exit_on_failure?
+    false
+  end
+end
